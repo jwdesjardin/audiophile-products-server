9509d393fb867a8d9807a2083e4a59fd8c72d514
joey desjardins <jwdesjardin@gmail.com>
2021-06-22 14:16:09 -0700

    copy simple ts stater express API

---
+++ server.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+console.log('server', process.env.SERVER_API_KEY)
+
+const app = express()
+
+express.json()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+	try {
+		res.send('Welcome to the NHL Stats API')
+	} catch (error) {
+		console.log('error getting team standings')
+	}
+})
+
+app.get('/api/heywhatsup', (req, res) => {
+	console.log(req)
+	res.send('nothing much wbu')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`)
+})
